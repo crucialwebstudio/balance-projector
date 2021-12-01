40060cc18a3fd8586fa60b6d4fe658232abc6839
Dan Bowen <dan@crucialwebstudio.com>
2021-11-30 18:52:32 -0700

    Print table of running balances

--- src/balance_projector/__main__.py
+++ src/balance_projector/__main__.py
@@ -2,7 +2,10 @@
 import click
 import os
 import yaml
+from tabulate import tabulate
 from .projector import Projector
+
+DATE_FORMAT = '%Y-%m-%d'
 
 
 def get_yaml():
@@ -22,15 +25,24 @@ def cli():
 )
 @click.option('--account-id', type=click.INT, required=True, help='Account Id to project.')
 @click.option('--starting-balance', type=click.FLOAT, required=True, help='Starting balance of the account.')
-@click.option('--start-date', type=click.DateTime(formats=['%Y-%m-%d']), required=True, help='Start date.')
-@click.option('--end-date', type=click.DateTime(formats=['%Y-%m-%d']), required=True, help='End date.')
+@click.option('--start-date', type=click.DateTime(formats=[DATE_FORMAT]), required=True, help='Start date.')
+@click.option('--end-date', type=click.DateTime(formats=[DATE_FORMAT]), required=True, help='End date.')
 def project(account_id, starting_balance, start_date, end_date):
     spec = get_yaml()
     projector = Projector.from_spec(spec)
     balances = projector.project(account_id, starting_balance,
-                                 start_date.strftime('%Y-%m-%d'),
-                                 end_date.strftime('%Y-%m-%d'))
-    print(balances)
+                                 start_date.strftime(DATE_FORMAT),
+                                 end_date.strftime(DATE_FORMAT))
+    headers = ['Date', 'Name', 'Amount', 'Balance']
+    table_data = [
+        [
+            b.transaction.date.strftime(DATE_FORMAT),
+            b.transaction.name,
+            b.transaction.amount,
+            b.balance
+        ] for b in balances
+    ]
+    click.echo(tabulate(table_data, headers=headers))
 
 
 if __name__ == '__main__':
